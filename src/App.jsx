886fdeef15c7725f5c1268f24a5135f33f24915f
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-05-17 12:13:54 -0400

    Console log pokemon images

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
 // import { useState } from 'react'
+import Cards from './Cards'
 
 function App() {
   return (
     <>
       <h1>Memory Card</h1>
+      <Cards />
     </>
   )
 }
---
+++ src/Cards.jsx
@@ -0,0 +1,17 @@
+async function Cards() {
+  const response = await fetch('https://pokeapi.co/api/v2/pokemon?limit=6')
+
+  const data = await response.json()
+
+  const promises = data.results.map((pokemon) =>
+    fetch(pokemon.url).then((res) => res.json())
+  )
+
+  const pokemonDetails = await Promise.all(promises)
+
+  const images = pokemonDetails.map((pokemon) => pokemon.sprites.front_default)
+
+  console.log(images)
+}
+
+export default Cards
