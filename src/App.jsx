18a7269252f48c2e0631df31a1626d864249ff05
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-05-17 13:08:01 -0400

    Shuffle images on initial mount

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,19 @@
 import { useState, useEffect } from 'react'
 import Cards from './Cards'
 
+const shuffleArray = (array) => {
+  return array.sort(() => Math.random() - 0.5)
+}
+
 function App() {
   const [cards, setCards] = useState([])
 
   useEffect(() => {
     const initializeGame = async () => {
       const images = await Cards()
-      setCards(images.map((image, index) => ({ id: index, image })))
+      setCards(
+        shuffleArray(images.map((image, index) => ({ id: index, image })))
+      )
     }
 
     initializeGame()
