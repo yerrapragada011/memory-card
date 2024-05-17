a031476083f985d980d0e7b7fbf6f779abf45ed0
yerrapragada011 <yerrapragada.agasthya@gmail.com>
2024-05-17 13:40:20 -0400

    Make each image a button and add game logic

--- src/App.jsx
+++ src/App.jsx
@@ -8,6 +8,9 @@ const shuffleArray = (array) => {
 
 function App() {
   const [cards, setCards] = useState([])
+  const [lastClickedCard, setLastClickedCard] = useState(null)
+  const [score, setScore] = useState(0)
+  const [bestScore, setBestScore] = useState(0)
 
   useEffect(() => {
     const initializeGame = async () => {
@@ -20,13 +23,34 @@ function App() {
     initializeGame()
   }, [])
 
+  const handleClick = (index) => {
+    const selectedCard = cards[index]
+
+    if (lastClickedCard && lastClickedCard.image === selectedCard.image) {
+      setScore(0)
+      setLastClickedCard(null)
+    } else {
+      setScore(score + 1)
+      if (score + 1 > bestScore) {
+        setBestScore(score + 1)
+      }
+      setLastClickedCard(selectedCard)
+    }
+
+    setCards(shuffleArray([...cards]))
+  }
+
   return (
     <>
       <div className='game'>
         <h1>Memory Card</h1>
+        <p>Score: {score}</p>
+        <p>Best Score: {bestScore}</p>
         <div className='card-grid'>
           {cards.map((card, index) => (
-            <img src={card.image} key={index} />
+            <button key={index} onClick={() => handleClick(index)}>
+              <img src={card.image} />
+            </button>
           ))}
         </div>
       </div>
